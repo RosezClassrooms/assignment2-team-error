129b53953a525b3fae4d9122b387485d056e1014
repl.it user <replituser@example.com>
2020-09-24 21:32:51 +0000

    Last Push

--- robot_with_builder.py
+++ robot_with_builder.py
@@ -1,190 +1,125 @@
 from abc import ABC, abstractmethod  # For Builder classes
+from enum import Enum
 
+RobotTraversal = Enum('RobotTraversal', 'bipedal quadripedal wheeled flying')
+RobotTraversalSystem = Enum('RobotTraversalSystem', 'bipedal_legs quadripedal_legs wings blades two_wheels four_wheels arms')
+RobotDetectionSystem = Enum('RobotDetectionSystem', 'camera_detection_system infrared_detection_system')
 # Doesn't need an endless list of arguments when initialized
 class Robot:
-  # Uses a lot of flag logic here:  Is that necessary?
-  # Does the use of this flag logic create other problems?
-  def __init__(self):
-    self.bipedal = False
-    self.quadripedal = False
-    self.wheeled = False
-    self.flying = False
-    self.traversal = []
-    self.detection_systems = []
+    # Uses a lot of flag logic here:  Is that necessary?
+    # Does the use of this flag logic create other problems?
 
-  # Huge decision statement: why is this not good?
-  # Can we improve this?
-  def __str__(self):
-    string = ""
-    if self.bipedal:
-      string += "BIPEDAL "
-    if self.quadripedal:
-      string += "QUADRIPEDAL "
-    if self.flying:
-      string += "FLYING ROBOT "
-    if self.wheeled:
-      string += "ROBOT ON WHEELS\n"
-    else:
-      string += "ROBOT\n"
+    def __init__(self):
+        self.traversal = None
+        self.traversal_systems = []
+        self.detection_systems = []
 
-    if self.traversal:
-      string += "Traversal modules installed:\n"
+    def __str__(self):
+        
+        string = "Robot is " + str(self.traversal.name) + "\n"  
 
-    for module in self.traversal:
-      string += "- " + str(module) + "\n"
+        if self.traversal_systems:
+          string += "Traversal modules installed:\n"
+        i = 0
+        for module in self.traversal_systems:
+            if(type(module) == list):
+              string += "- " + str(module.name[i]) + "\n"
+              i += 1
+            else:
+              string += "- " + str(module.name) + "\n"
+        i = 0          
+        if self.detection_systems:
+          string += "Detection systems installed:\n"
 
-    if self.detection_systems:
-      string += "Detection systems installed:\n"
+        for system in self.detection_systems:
+            if(type(system) == list):
+              string += "- " + str(system[i].name) + "\n"
+              i += 1
+            else:
+              string += "- " + str(system.name) + "\n"
 
-    for system in self.detection_systems:
-      string += "- " + str(system) + "\n"
+        return string
 
-    return string
-
-#---------------------------------------------------------------------------
-
-# Concrete classes for componenets
-# In a real application, there would be an endless list of these, each one
-#   composing additional subcomponents
-class BipedalLegs:
-  def __str__(self):
-    return "two legs"
-
-class QuadripedalLegs:
-  def __str__(self):
-    return "four legs"
-
-class Arms:
-  def __str__(self):
-    return "two arms"
-
-class Wings:
-  def __str__(self):
-    return "wings"
-
-class Blades:
-  def __str__(self):
-    return "blades"
-
-class FourWheels:
-  def __str__(self):
-    return "four wheels"
-
-class TwoWheels:
-  def __str__(self):
-    return "two wheels"
-
-class CameraDetectionSystem:
-  def __str__(self):
-    return "cameras"
-
-class InfraredDetectionSystem:
-  def __str__(self):
-    return "infrared"
-
-#----------------------------------------------------------------------------
-# Note that this code was place at the top of this program for visibility
-#from abc import ABC, abstractmethod
 
 # The abstract superclass for all the builders
 # We're using inheritence, but it's shallow
 class RobotBuilder(ABC):
-    
-  @abstractmethod
-  def reset(self):
-    pass
+    @abstractmethod
+    def reset(self):
+        pass
 
-  @abstractmethod
-  def build_traversal(self):
-    pass
+    @abstractmethod
+    def build_traversal(self):
+        pass
 
-  @abstractmethod
-  def build_detection_system(self):
-    pass
+    @abstractmethod
+    def build_detection_system(self):
+        pass
 
-    
+
 # Concrete Builder class:  there would be MANY of these
 class AndroidBuilder(RobotBuilder):
-  def __init__(self):
-    self.product = Robot()
+    def __init__(self):
+        self.product = Robot()
 
-  def reset(self):
-    self.product = Robot()
+    def reset(self):
+        self.product = Robot()
 
-  # All of the concrete builders have this in common
-  # Should it be elevated to the superclass?  
-  def get_product(self):
-    return self.product
+    # All of the concrete builders have this in common
+    # Should it be elevated to the superclass?
+    def get_product(self):
+        return self.product
 
-  def build_traversal(self):
-    self.product.bipedal = True
-    self.product.traversal.append(BipedalLegs())
-    self.product.traversal.append(Arms())
+    def build_traversal(self):
+        traversal_items = (RobotTraversalSystem.bipedal_legs, RobotTraversalSystem.arms)
+        self.product.traversal_systems.append(RobotTraversalSystem.bipedal_legs)
+        self.product.traversal_systems.append(RobotTraversalSystem.arms)
+        self.product.traversal = RobotTraversal.bipedal
 
-  def build_detection_system(self):
-    self.product.detection_systems.append(CameraDetectionSystem())
+
+    def build_detection_system(self):
+        self.product.detection_systems.append(RobotDetectionSystem.camera_detection_system)
+
+
 
 # Concrete Builder class:  there would be many of these
 class AutonomousCarBuilder(RobotBuilder):
-  def __init__(self):
-    self.product = Robot()
+    def __init__(self):
+        self.product = Robot()
 
-  def reset(self):
-    self.product = Robot()
+    def reset(self):
+        self.product = Robot()
 
-  # All of the concrete builders have this in common
-  # Should it be elevated to the superclass?  
-  def get_product(self):
-    return self.product
+    # All of the concrete builders have this in common
+    # Should it be elevated to the superclass?
+    def get_product(self):
+        return self.product
 
-  def build_traversal(self):
-    self.product.wheeled = True
-    self.product.traversal.append(FourWheels())
+    def build_traversal(self):
+        self.product.traversal_systems.append(RobotTraversalSystem.four_wheels)
+        self.product.traversal = RobotTraversal.wheeled
 
-  def build_detection_system(self):
-    self.product.detection_systems.append(InfraredDetectionSystem())
+    def build_detection_system(self):
 
-#-------------------------------------------------------------------------
-#'''
-# Remove # in line above to comment out this section when using Director
+        self.product.detection_systems.append(RobotDetectionSystem.infrared_detection_system)
 
-# Using the builders to create different robots
-builder = AndroidBuilder()
-builder.build_traversal()
-builder.build_detection_system()
-print(builder.get_product())
 
-builder = AutonomousCarBuilder()
-builder.build_traversal()
-builder.build_detection_system()
-print(builder.get_product())
-
-#-------------------------------------------------------
-#  Keep line below whether testing builders or director
-'''
-#-------------------------------------------------------
 
 # Diretor manages all of the Builders
-# Do we need separate make methods?
+# Created one make_robot method for all builders since the builder type i different
 class Director:
-    def make_android(self, builder):
+    def make_robot(self,builder):
         builder.build_traversal()
         builder.build_detection_system()
         return builder.get_product()
 
-    def make_autonomous_car(self, builder):
-        builder.build_traversal()
-        builder.build_detection_system()
-        return builder.get_product()
 
 director = Director()
 
 builder = AndroidBuilder()
-print(director.make_android(builder))
+print(director.make_robot(builder))
 
 builder = AutonomousCarBuilder()
-print(director.make_autonomous_car(builder))
+print(director.make_robot(builder))
 
 # comment out line below when testing director
-'''
-
